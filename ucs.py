2b0bc25aed7b1b00cd77889046f6c0e6e4910c93
Tạ Ngọc Tú <tu.tn215503@sis.hust.edu.vn>
2023-11-12 01:18:09 +0700

    updte

--- ucs.py
+++ ucs.py
@@ -16,6 +16,7 @@ def algorithm_ucs(grap, start_point, end_point):
                     new_cost = cost + weight
                     heapq.heappush(queue, (new_cost, new_path))
     return 0, []
+
 def ucs(graph :dict, start_point: dict, end_point: dict) -> list:
     start_node = start_point.get('id')
     goal_node = end_point.get('id')
